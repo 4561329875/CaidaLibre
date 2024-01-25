6539dff1c8c46cfc674d1759443e1e468f4230a2
David <fdtoro@espe.edu.ec>
2024-01-25 12:15:18 -0500

    MongoDB codigo sin provar

--- src/main/java/backend/Cuadrado.java
+++ src/main/java/backend/Cuadrado.java
@@ -4,6 +4,20 @@
  */
 package backend;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import java.util.ArrayList;
+import java.util.List;
+import org.bson.Document;
+
 /**
  *
  * @author david
@@ -23,4 +37,108 @@ public class Cuadrado extends Figura {
     }
     
     
+    @Override
+    public void guardarBDD() {
+        String connectionString = "mongodb+srv://fdtoro:WjykdQ47BQOqZoDm@cluster0.mgdhlmc.mongodb.net/?retryWrites=true&w=majority";
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("figurasBDD");
+
+                MongoCollection<Document> collection = database.getCollection("Cuadrados");
+
+                Document document = new Document("lado", this.lado);
+                        
+                        
+
+                collection.insertOne(document);
+
+                mongoClient.close();
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    @Override
+    public void eliminarAreas() {
+        String connectionString = "mongodb+srv://fdtoro:WjykdQ47BQOqZoDm@cluster0.mgdhlmc.mongodb.net/?retryWrites=true&w=majority";
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("figurasBDD");
+
+                MongoCollection<Document> collection = database.getCollection("Cuadrados");
+
+                collection.deleteMany(new Document());
+
+                mongoClient.close();
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public Cuadrado[] consultarAreas() {
+
+        List<Cuadrado> lista = new ArrayList<Cuadrado>();
+        Cuadrado[] array = null;
+
+        String connectionString = "mongodb+srv://fdtoro:WjykdQ47BQOqZoDm@cluster0.mgdhlmc.mongodb.net/?retryWrites=true&w=majority";
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("figurasBDD");
+
+                MongoCollection<Document> collection = database.getCollection("Cuadrados");
+
+                
+
+                try (MongoCursor<Document> cursor = collection.find().iterator()) {
+                    while (cursor.hasNext()) {
+                        Document document = cursor.next();
+                        Cuadrado tri;
+                        tri = new Cuadrado(document.getDouble("lado"));
+                        tri.calcularArea();
+                        lista.add(tri);
+
+                    }
+                }
+
+                array = lista.toArray(new Cuadrado[0]);
+                
+                mongoClient.close();
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return array;
+    }
 }
--- src/main/java/backend/Figura.java
+++ src/main/java/backend/Figura.java
@@ -4,15 +4,22 @@
  */
 package backend;
 
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 /**
  *
  * @author david
  */
 public class Figura {
+
     private double area;
-    
-    public double  calcularArea(){
-    return 0;
+
+    public double calcularArea() {
+        return 0;
     }
 
     public double getArea() {
@@ -23,6 +30,14 @@ public class Figura {
         this.area = area;
     }
 
+    public void guardarBDD() {
+        
+
+    }
+    
+    public void eliminarAreas(){
     
     
+    }
+
 }
